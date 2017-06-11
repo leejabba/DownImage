577509a3ec78884224706a8fac7549545cda6e4a
leejabba <dlgodrhs@gmail.com>
2017-06-11 23:15:42 +0900

    로컬에 파일을 다운로드 받아 저장해 놓고 파일을 열어 보여주는 예제. 파일이 없을 때만 주소로 접속해 다운로드 받으므로 최초 접속시에 한 번만 네트워크에 연결한다. 이후에는 받아놓은 이미지를 사용하므로 항상 접속하는 것과 비교해 속도가 훨씬 빠르다.

--- app/src/main/java/kr/heythisway/downimage/MainActivity.java
+++ app/src/main/java/kr/heythisway/downimage/MainActivity.java
@@ -1,8 +1,8 @@
 package kr.heythisway.downimage;
 
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -11,8 +11,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,47 +31,85 @@ public class MainActivity extends AppCompatActivity {
         btnDown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new DownThread("http://ppss.kr/wp-content/uploads/2015/05/창업성공51.jpg").start();
+                String imageUrl = "http://www.soen.kr/data/child3.jpg";
+                // url에서 경로를 빼고 파일명만 추출하여 전달하는 과정
+                int idx = imageUrl.lastIndexOf('/');
+                String localImage = imageUrl.substring(idx + 1);
+                String path = Environment.getDataDirectory().getAbsolutePath();
+                path = path + "/data/kr.heythisway.downimage/files/" + localImage;
+
+                // 파일이 없을때만 네트워크에 연결해 다운로드 받고 이미지뷰에 출력한다.
+                if (new File(path).exists()) {
+                    Toast.makeText(MainActivity.this, "비트맵이 존재합니다.", Toast.LENGTH_SHORT).show();
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+                } else {
+                    Toast.makeText(MainActivity.this, "비트맵이 존재하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    (new DownThread(imageUrl, localImage)).start();
+                }
             }
 
+            // 핸들러
+            Handler mAfterDown = new Handler() {
+                public void handleMessage(Message msg) {
+                    if (msg.obj != null) {
+                        String path = Environment.getDataDirectory().getAbsolutePath();
+                        path = path + "/data/kr.heythisway.downimage/files/" + msg.obj;
+                        imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+                    } else {
+                        Toast.makeText(MainActivity.this, "파일을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+            
+            // 커스텀 스레드 클래스 설정
             class DownThread extends Thread {
                 String addr;
 
-                public DownThread(String addr) {
+                String fileName;
+
+                public DownThread(String addr, String fileName) {
                     this.addr = addr;
+                    this.fileName = fileName;
                 }
 
                 @Override
                 public void run() {
                     super.run();
+                    URL imageUrl;
+                    int read;
+
                     try {
-                        // URL 객체로부터 스트림을 열고
-                        InputStream is = new URL(addr).openStream();
-                        // BitmapFactory의 decodeStream 메서드를 호출하면
-                        // 원격지의 이미지 스트림을 읽어 비트맵 객체로 변환한다.
-                        Bitmap bit = BitmapFactory.decodeStream(is);
-                        is.close();
-
-                        // 변환된 비트맵을 핸들러로 보내 레이아웃의 이미지뷰에 출력
-                        Message message = mAfterDown.obtainMessage();
-                        // 비트맵 값을 메시지로 넘기기 위해 오브젝트 형식으로 담아보낸다.
-                        message.obj = bit;
-                        mAfterDown.sendMessage(message);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                Handler mAfterDown = new Handler() {
-                    public void handleMessage(Message msg) {
-                        Bitmap bit = (Bitmap) msg.obj;
-                        if (bit == null) {
-                            Toast.makeText(MainActivity.this, "bitmap is null", Toast.LENGTH_SHORT).show();
-                        } else {
-                            imageView.setImageBitmap(bit);
+                        imageUrl = new URL(addr);
+                        HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
+                        // getContentLength는 응답 결과의 크기를 바이트 단위로 조사하는데
+                        int len = conn.getContentLength();
+                        // 조사된 길이만큼 바이트 배열을 할당하고 이 배열에 읽어들인다.
+                        byte[] raster = new byte[len];
+                        // 로컬 파일 스트림에 기록하여 복사
+                        InputStream is = conn.getInputStream();
+                        FileOutputStream fos = openFileOutput(fileName, 0);
+                        
+                        for (;;) {
+                            read = is.read(raster);
+                            if (read <= 0) {
+                                break;
+                            }
+                            fos.write(raster, 0, read);
                         }
+                        
+                        is.close();
+                        fos.close();
+                        conn.disconnect();
+                        
+                    } catch (java.io.IOException e) {
+                        fileName = null;
                     }
-                };
+
+                    // 다운로드에 성공했으면 핸들러로 신호를 보내 로컬에 저장된 비트맵을 이미지뷰에 출력
+                    Message msg = mAfterDown.obtainMessage();
+                    msg.obj = fileName;
+                    mAfterDown.sendMessage(msg);
+                }
             }
         });
     }
